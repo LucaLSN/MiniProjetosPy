104dd4e4b4f4c4679ea4a2db7082cd8ad5f9f60f
Lucas da Silva <90360265+LucaLSN@users.noreply.github.com>
2023-06-27 16:20:40 -0300

    Atualizar o conversor_temperatura.py

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,3 +1,12 @@
-print('Conversor de temperatura')
-fah: float(input('Por favor digite a temperaturam em Fº: ))
-                 
+import time 
+redtext='\033[91m'
+
+print("Bem vindo ao Conversor de Temperatura F°>C°")
+time.sleep(1)
+print("\33[91m\33[1mPor favor, digite a temperatura em F°")
+tempf= float(input("Temperatura:"))
+tempc = round(((tempf-32)*5/9),1)
+time.sleep(2)
+print("\33[93mCalculando...")
+time.sleep(3)
+print(f"Temperatura digitada F°: {tempf} equivale a {tempc} Graus Celcius")
